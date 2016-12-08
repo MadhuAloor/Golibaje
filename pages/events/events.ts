a4d4b9f135f8657952f9abedc43df3061cb4a4b1
Madhukeshwara <madhu.aloor@gmail.com>
2016-12-08 20:07:55 +0530

    Create events.ts

---
+++ pages/events/events.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Events page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-events',
+  templateUrl: 'events.html'
+})
+export class EventsPage {
+  private items:any = [];
+  constructor(public navCtrl: NavController) {this.items =[1,2,3,4];}
+
+
+  ionViewDidLoad() {
+    console.log('Hello EventsPage Page');
+  }
+
+}
